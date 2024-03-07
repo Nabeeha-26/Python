a29a6d8198a784cfe6392b59c07464241fa92518
Nabeeha-26 <100037246+Nabeeha-26@users.noreply.github.com>
2024-03-07 21:59:37 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,9 @@
+# Example input lists
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+
+# Print positive numbers in list1
+print("Input:", list1, "Output:", [num for num in list1 if num > 0])
+
+# Print positive numbers in list2
+print("Input:", list2, "Output:", [num for num in list2 if num > 0])
