6f74c207b9d1e951602cb03db74075ecc1ea897d
Nabeeha-26 <100037246+Nabeeha-26@users.noreply.github.com>
2024-03-07 23:19:14 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,19 @@
+# Given sets
+E = {0, 1, 2, 3, 4, 5, 6, 8}
+N = {2, 4}
+
+# Union of E and N
+union_result = E.union(N)
+print("Union:", union_result)
+
+# Intersection of E and N
+intersection_result = E.intersection(N)
+print("Intersection:", intersection_result)
+
+# Difference of E and N
+difference_result = E.difference(N)
+print("Difference:", difference_result)
+
+# Symmetric difference of E and N
+symmetric_difference_result = E.symmetric_difference(N)
+print("Symmetric Difference:", symmetric_difference_result)
